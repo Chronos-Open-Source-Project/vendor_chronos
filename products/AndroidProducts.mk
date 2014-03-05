c86b7b20a500fde258106c6dffd79240a5be2416
Rob <robczepil@gmail.com>
2014-03-04 22:04:20 -0800

    Add jflte to AndroidProducts.mk & remove unused make files

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,7 +1,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/hercules.mk \
-    $(LOCAL_DIR)/jfltetmo.mk \
-    $(LOCAL_DIR)/jfltecan.mk \
-    $(LOCAL_DIR)/jflteatt.mk \
+    $(LOCAL_DIR)/jflte.mk \
     $(LOCAL_DIR)/mako.mk \
 
